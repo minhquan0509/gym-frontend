995245b55be5a46785434d066fef0608f502e202
minhquan0509 <minkkquan2@gmail.com>
2023-05-28 17:06:59 +0700

    initializing

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header'
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <div className="homepage">
+        <p>Homepage</p>
+      </div>
+    </>
+  )
+}
+
+export default Homepage
