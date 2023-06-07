1841b1c610db58ed3ab8ca9ac40fb6005c475833
Plysn <phuonglynguyen61@gmail.com>
2023-06-07 11:47:51 +0700

    [Gym Detail] add owner button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css'
-import GymDetail from './pages/GymDetail'
-import Homepage from './pages/Homepage'
-import GymReview from './pages/GymReview'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import SearchPage from './pages/SearchPage'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import "./App.css";
+import GymDetail from "./pages/GymDetail";
+import Homepage from "./pages/Homepage";
+import GymReview from "./pages/GymReview";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
