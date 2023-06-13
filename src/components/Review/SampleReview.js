4515e2a537ea007f5cad1911a6db2491611116c0
unknown <ngodieu10102001@gmail.com>
2023-06-13 22:53:03 +0700

    update UI review page

--- src/components/Review/SampleReview.js
+++ src/components/Review/SampleReview.js
@@ -14,6 +14,8 @@ import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
 import "../../css/review.css";
 import axios from "axios";
 import { useEffect } from "react";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 function SampleReview() {
   // useEffect(() => {
@@ -62,207 +64,259 @@ function SampleReview() {
 
   return (
     <>
-      <Box
-        sx={{ display: "flex", alignItems: "center" }}
-        className="review-item"
-      >
-        <Avatar src={sampleAvatarUrl} alt="Avatar" className="avatarUser" />
-        <Box sx={{ ml: 2 }}>
-          <div>Username</div>
-          <Rating
-            name="sample-rating"
-            value={sampleRatingValue}
-            readOnly
-            size="small"
-          />
-          <Typography variant="body1" gutterBottom className="review-text">
-            {sampleText}
-          </Typography>
-          <Grid>
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
+      <Box className="review_list-item">
+        <div className="review_list-user-content">
+          <div className="review_list-user-infor">
+            <Avatar src={sampleAvatarUrl} alt="Avatar" className="review_list-user-infor-avatarUser" />
+            <div style={{ textAlign: 'center' }} className="review_list-user-infor-name" >名前</div>
+            <Rating
+              name="sample-rating"
+              value={sampleRatingValue}
+              readOnly
+              size="small"
             />
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
-            />
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
-            />
-          </Grid>
-          <IconButton
-            onClick={handleLike}
-            className="like-btn"
-            size="small"
-            sx={{ color: liked ? "blue" : "gray" }}
-          >
-            {liked ? (
-              <ThumbUpAlt fontSize="small" />
-            ) : (
-              <ThumbUpOffAltIcon fontSize="small" />
-            )}
-            <span className="like-count">{likeCount}</span>
-          </IconButton>
-          <IconButton
-            onClick={handleDislike}
-            className="like-btn"
-            size="small"
-            sx={{ color: disliked ? "red" : "gray" }}
-          >
-            {disliked ? (
-              <ThumbDownAltIcon fontSize="small" />
-            ) : (
-              <ThumbDownOffAltIcon fontSize="small" />
-            )}
-            <span className="like-count">{dislikeCount}</span>
-          </IconButton>
-        </Box>
+          </div>
+          <Box sx={{ ml: 3 }} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography gutterBottom>
+              きれいなジムです。トレーナーも親切で、いいジムです。
+            </Typography>
+            <Grid>
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+            </Grid>
+          </Box>
+        </div>
+        <div className="review_list-star-time-react">
+          <div>
+            <div>
+              <label>プール：</label>
+              <Rating
+                name="sample-rating"
+                value={sampleRatingValue}
+                readOnly
+                size="small" />
+            </div>
+            <div>投稿日：2023年6月20日</div>
+          </div>
+          <div>
+            <IconButton
+              onClick={handleLike}
+              className="review_list-like-btn"
+              size="small"
+              sx={{ color: liked ? "blue" : "gray" }}
+            >
+              {liked ? (
+                <ThumbUpAlt fontSize="small" />
+              ) : (
+                <ThumbUpOffAltIcon fontSize="small" />
+              )}
+              <span className="like-count">{likeCount}</span>
+            </IconButton>
+            <IconButton
+              onClick={handleDislike}
+              className="review_list-like-btn"
+              size="small"
+              sx={{ color: disliked ? "red" : "gray" }}
+            >
+              {disliked ? (
+                <ThumbDownAltIcon fontSize="small" />
+              ) : (
+                <ThumbDownOffAltIcon fontSize="small" />
+              )}
+              <span className="like-count">{dislikeCount}</span>
+            </IconButton>
+          </div>
+        </div>
       </Box>
-      <Box
-        sx={{ display: "flex", alignItems: "center" }}
-        className="review-item"
-      >
-        <Avatar src={sampleAvatarUrl} alt="Avatar" className="avatarUser" />
-        <Box sx={{ ml: 2 }}>
-          <div>Username</div>
-          <Rating
-            name="sample-rating"
-            value={sampleRatingValue}
-            readOnly
-            size="small"
-          />
-          <Typography variant="body1" gutterBottom className="review-text">
-            {sampleText}
-          </Typography>
-          <Grid>
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
+      <Box className="review_list-item">
+        <div className="review_list-user-content">
+          <div className="review_list-user-infor">
+            <Avatar src={sampleAvatarUrl} alt="Avatar" className="review_list-user-infor-avatarUser" />
+            <div style={{ textAlign: 'center' }} className="review_list-user-infor-name" >名前</div>
+            <Rating
+              name="sample-rating"
+              value={sampleRatingValue}
+              readOnly
+              size="small"
             />
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
-            />
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
-            />
-          </Grid>
-          <IconButton
-            onClick={handleLike}
-            className="like-btn"
-            size="small"
-            sx={{ color: liked ? "blue" : "gray" }}
-          >
-            {liked ? (
-              <ThumbUpAlt fontSize="small" />
-            ) : (
-              <ThumbUpOffAltIcon fontSize="small" />
-            )}
-            <span className="like-count">{likeCount}</span>
-          </IconButton>
-          <IconButton
-            onClick={handleDislike}
-            className="like-btn"
-            size="small"
-            sx={{ color: disliked ? "red" : "gray" }}
-          >
-            {disliked ? (
-              <ThumbDownAltIcon fontSize="small" />
-            ) : (
-              <ThumbDownOffAltIcon fontSize="small" />
-            )}
-            <span className="like-count">{dislikeCount}</span>
-          </IconButton>
-        </Box>
+          </div>
+          <Box sx={{ ml: 3 }} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography gutterBottom>
+              きれいなジムです。トレーナーも親切で、いいジムです。
+            </Typography>
+            <Grid>
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+            </Grid>
+          </Box>
+        </div>
+        <div className="review_list-star-time-react">
+          <div>
+            <div>
+              <label>プール：</label>
+              <Rating
+                name="sample-rating"
+                value={sampleRatingValue}
+                readOnly
+                size="small" />
+            </div>
+            <div>投稿日：2023年6月20日</div>
+          </div>
+          <div>
+            <IconButton
+              onClick={handleLike}
+              className="review_list-like-btn"
+              size="small"
+              sx={{ color: liked ? "blue" : "gray" }}
+            >
+              {liked ? (
+                <ThumbUpAlt fontSize="small" />
+              ) : (
+                <ThumbUpOffAltIcon fontSize="small" />
+              )}
+              <span className="like-count">{likeCount}</span>
+            </IconButton>
+            <IconButton
+              onClick={handleDislike}
+              className="review_list-like-btn"
+              size="small"
+              sx={{ color: disliked ? "red" : "gray" }}
+            >
+              {disliked ? (
+                <ThumbDownAltIcon fontSize="small" />
+              ) : (
+                <ThumbDownOffAltIcon fontSize="small" />
+              )}
+              <span className="like-count">{dislikeCount}</span>
+            </IconButton>
+          </div>
+        </div>
       </Box>
-      <Box
-        sx={{ display: "flex", alignItems: "center" }}
-        className="review-item"
-      >
-        <Avatar src={sampleAvatarUrl} alt="Avatar" className="avatarUser" />
-        <Box sx={{ ml: 2 }}>
-          <div>Username</div>
-          <Rating
-            name="sample-rating"
-            value={sampleRatingValue}
-            readOnly
-            size="small"
-          />
-          <Typography variant="body1" gutterBottom className="review-text">
-            {sampleText}
-          </Typography>
-          <Grid>
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
+      <Box className="review_list-item">
+        <div className="review_list-user-content">
+          <div className="review_list-user-infor">
+            <Avatar src={sampleAvatarUrl} alt="Avatar" className="review_list-user-infor-avatarUser" />
+            <div style={{ textAlign: 'center' }} className="review_list-user-infor-name" >名前</div>
+            <Rating
+              name="sample-rating"
+              value={sampleRatingValue}
+              readOnly
+              size="small"
             />
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
-            />
-            <img
-              src={sampleImageUrl}
-              alt="Review Image"
-              width="80"
-              height="80"
-              className="reviewedImg"
-            />
-          </Grid>
-          <IconButton
-            onClick={handleLike}
-            className="like-btn"
-            size="small"
-            sx={{ color: liked ? "blue" : "gray" }}
-          >
-            {liked ? (
-              <ThumbUpAlt fontSize="small" />
-            ) : (
-              <ThumbUpOffAltIcon fontSize="small" />
-            )}
-            <span className="like-count">{likeCount}</span>
-          </IconButton>
-          <IconButton
-            onClick={handleDislike}
-            className="like-btn"
-            size="small"
-            sx={{ color: disliked ? "red" : "gray" }}
-          >
-            {disliked ? (
-              <ThumbDownAltIcon fontSize="small" />
-            ) : (
-              <ThumbDownOffAltIcon fontSize="small" />
-            )}
-            <span className="like-count">{dislikeCount}</span>
-          </IconButton>
-        </Box>
+          </div>
+          <Box sx={{ ml: 3 }} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography gutterBottom>
+              きれいなジムです。トレーナーも親切で、いいジムです。
+            </Typography>
+            <Grid>
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+              <img
+                src={sampleImageUrl}
+                alt="Review Image"
+                width="80"
+                height="80"
+                className="reviewedImg"
+              />
+            </Grid>
+          </Box>
+        </div>
+        <div className="review_list-star-time-react">
+          <div>
+            <div>
+              <label>プール：</label>
+              <Rating
+                name="sample-rating"
+                value={sampleRatingValue}
+                readOnly
+                size="small" />
+            </div>
+            <div>投稿日：2023年6月20日</div>
+          </div>
+          <div>
+            <IconButton
+              onClick={handleLike}
+              className="review_list-like-btn"
+              size="small"
+              sx={{ color: liked ? "blue" : "gray" }}
+            >
+              {liked ? (
+                <ThumbUpAlt fontSize="small" />
+              ) : (
+                <ThumbUpOffAltIcon fontSize="small" />
+              )}
+              <span className="like-count">{likeCount}</span>
+            </IconButton>
+            <IconButton
+              onClick={handleDislike}
+              className="review_list-like-btn"
+              size="small"
+              sx={{ color: disliked ? "red" : "gray" }}
+            >
+              {disliked ? (
+                <ThumbDownAltIcon fontSize="small" />
+              ) : (
+                <ThumbDownOffAltIcon fontSize="small" />
+              )}
+              <span className="like-count">{dislikeCount}</span>
+            </IconButton>
+          </div>
+        </div>
       </Box>
+
+      <Stack spacing={2} className="review_list-pagination">
+        <Pagination count={10} shape="rounded" />
+      </Stack>
     </>
   );
 }
